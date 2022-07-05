ebba7fb4d93047a1b3a6c94747824d3309edbf46
rupikaur123 <you@example.com>
2022-07-05 23:34:35 +0530

    Dashboard_list-api_implementation

--- src/app/components/dashboard/ecommerce/ecommerce.component.ts
+++ src/app/components/dashboard/ecommerce/ecommerce.component.ts
@@ -1,24 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import * as chartData from '../../../shared/data/dashboard/ecommerce'
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-ecommerce',
   templateUrl: './ecommerce.component.html',
   styleUrls: ['./ecommerce.component.scss']
 })
 export class EcommerceComponent implements OnInit {
-
-  public todayTotalSale = chartData.todayTotalSale;
-  public totalVisit = chartData.totalVisit;
-  public profit = chartData.profit;
-  public riskFactor = chartData.riskFactor;
-
-  public lat_m1: number =  20.593683;
-  public lng_m1: number = 78.962883;
-  public zoom_m1: number = 4;
-  
-  constructor() { }
+  dashboardList: any
+  res: any
+  baseUrl: any = 'http://api.gurdevhospital.co/'
+  token: any = ''
+  searchUser: any
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 7;
+  tableSizes: any = [3, 6, 9, 12];
+  constructor(public http: HttpClient, public toster: ToastrService) { }
 
   ngOnInit(): void {
+    this.token = localStorage.getItem('token')
+    this.getDashboardList()
   }
-
+  getDashboardList() {
+    const headers = { 'Authorization': 'Bearer ' + this.token }
+    this.http.get<any>(this.baseUrl + 'api/dashboard', { 'headers': headers })
+      .subscribe(data => {
+        console.log("Get completed sucessfully. The response received " + data);
+        this.res = data.data;
+        this.dashboardList = this.res
+        console.log('dashboardList', this.dashboardList)
+      },
+        error => {
+          console.log("failed with the errors", error.error);
+          if (error.error) {
+            this.toster.error(error.error.message);
+          } else {
+            this.toster.error('Something went wrong');
+          }
+        }
+      );
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+  }
 }
